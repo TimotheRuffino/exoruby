55493290f3ba4c1ac4ee61667a69860b38b55c16
TimotheRuffino <timothe.ruffino@gmail.com>
2020-04-07 23:04:57 +0200

    fin exo

--- pyramide.rb
+++ pyramide.rb
@@ -4,6 +4,8 @@ print ">"
 
 nombre = gets.chomp.to_i #l'utilisateur choisi le nombre / to.i = on annonce que c'est un int.
 
+nombre2 = nombre
+
 puts "Voici la pyramide :"
 
 
@@ -11,10 +13,13 @@ puts "Voici la pyramide :"
 i = 1
 
 while i <=nombre && i >= 1 && i <= 25 # tant que le i n'est pas inférieur ou égal au nombre tapé par l'utilisateur / à 25 la pyramide continue de grandir
-  puts "." * i
-  puts "#" * i # sachant que i = i + 1, quand on * (multiplie) par i à chaque étage un # apparait
+  print " " * nombre2 
+  puts "#" * i  # sachant que i = i + 1, quand on * (multiplie) par i à chaque étage un # apparait
   i = i + 1
-  if i > 25 
-    puts "Nombre d'étage limite !" # arrivé à 25 la pyramide arrêt de descendre
+  nombre2 = nombre2 - 1
+    
+    if i > 25 
+    puts "Nombre d'étage limite !" # arrivé à 25 la pyramide arrête de descendre
   end
+
 end 
